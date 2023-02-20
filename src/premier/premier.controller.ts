bf630c9ff7ad52506b22042076e0e7626c92fca7
omar-besbes <omar.besbes@insat.ucar.tn>
2023-02-20 19:44:16 +0100

    feat: premier module

---
+++ src/premier/premier.controller.ts
@@ -0,0 +1,29 @@
+import {Controller, Delete, Get, Patch, Post, Put} from '@nestjs/common';
+
+@Controller('premier')
+export class PremierController {
+  @Get()
+  get(): string {
+    return 'get';
+  }
+
+  @Post()
+  post(): string {
+    return 'post';
+  }
+
+  @Delete()
+  delete(): string {
+    return 'delete';
+  }
+
+  @Put()
+  put(): string {
+    return 'put';
+  }
+
+  @Patch()
+  patch(): string {
+    return 'patch';
+  }
+}
